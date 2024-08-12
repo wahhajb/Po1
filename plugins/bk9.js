86b8d2900e4c930bcd8bc2fb27f7bb1887110b1a
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-12 19:06:07 +0300

    Update bk9.js

--- plugins/bk9.js
+++ plugins/bk9.js
@@ -11,14 +11,14 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       participants: '0@s.whatsapp.net', 
       remoteJid: 'status@broadcast', 
       fromMe: false, 
-      id: '𝐙𝐞𝐙𝐨 𝐁𝐨𝐓 ᥫ᭡' 
+      id: 'ستيڤن 𝐁𝐨𝐓 ᥫ᭡' 
     }, 
     message: { 
       contactMessage: { 
         vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` 
       } 
     }, 
-    participant:'201508628077@s.whatsapp.net' 
+    participant:'527774838355@s.whatsapp.net' 
   };
 
   if (command === 'تخيل' || command ==='ارسم') {
@@ -52,13 +52,13 @@ lang = 'en';
         await conn.sendButton(
           m.chat,
           `لــقــد تــخــيــلــت : 「 ${text} 」`, 
-          '𝐙𝐞𝐙𝐨 𝐁𝐨𝐓 ᥫ᭡',
+          'ستيڤن 𝐁𝐨𝐓 ᥫ᭡',
           result.BK9,
           [['「 صــوره آخــرى 」', `${usedPrefix + command} ${text}`]],null,[['「 قــنــاتــي 」','https://whatsapp.com/channel/0029Vaflefp4Y9ljqmqllP3a']],m);
         m.react ('✅'); 
       }
     } catch (error) {
-      throw 'اســف لـم اســتـطـيـع تــخـيـل ذالــگ 🗿';
+      throw 'اســف لـم اســتـطـيـع تــخـيـل ذاالــگ 🗿';
     }
   } else if (command === 'كانيكي' || command ==='ناغي') {
     if (!text) throw 'خــطــأ!! 」\n ادخــل نــص لـكـي ارد عـلـيـك';
@@ -69,7 +69,7 @@ lang = 'en';
       const result = await response.json();
 
       if (result.status && result.BK9) {
-        conn.sendMessage(m.chat,{text: result.BK9 +'\n\n> 𝐙𝐞𝐙𝐨 𝐁𝐨𝐓 ᥫ᭡'}, { quoted: fakecontact });
+        conn.sendMessage(m.chat,{text: result.BK9 +'\n\n>ستيڤن 𝐁𝐨𝐓 ᥫ᭡'}, { quoted: fakecontact });
       } else {
         throw 'اســف لـم اجــد اجــابــه 🙂';
       }
@@ -88,7 +88,7 @@ lang = 'en';
         let response = await fetch(`${BK9}/ai/geminiimg?url=${uploadedImageUrl}&q=${text}`);
         let result = await response.json();
 
-        conn.sendMessage(m.chat, { text: result.BK9 +'\n\n> 𝐙𝐞𝐙𝐨 𝐁𝐨𝐓 ᥫ᭡' }, { quoted: fakecontact });
+        conn.sendMessage(m.chat, { text: result.BK9 +'\n\n> ستيڤن 𝐁𝐨𝐓 ᥫ᭡' }, { quoted: fakecontact });
       } catch (error) {
         throw 'اســف لـم اجــد اجــابــه 🙂';
       }
