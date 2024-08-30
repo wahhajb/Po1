3c4b51e85331a6790f29bbaba8822f5d923972a9
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-30 18:29:53 +0000

    Update Menu azrar.js

--- plugins/Menu azrar.js
+++ plugins/Menu azrar.js
@@ -36,14 +36,14 @@ conn.relayMessage(m.chat, { viewOnceMessage: { message: { interactiveMessage: { 
 > •رتــبــتــك : ${role}
 *• ──╾⊱﹝⚜️﹞⊰╼── •*
 ╗───¤مــعلــومــات الــ🤖بــوت
-> •إســم الــبــوت: -شـيخ⁵¹⁷. 
-> •الــمــطــور :-شـيخ⁵¹⁷.
+> •إســم الــبــوت: -ستيفن⁵¹. 
+> الــمــطــور :-ستيڤن⁵.
 > •مــدة الــتــشــغــيل : ${uptime}
 *• ──╾⊱﹝⚜️﹞⊰╼── •*
 *~⌬ تــ✍︎ــوقــيــع ↡~*🖋️
 *⌞⚜️┊ス M.Z┊⚜️⌝*
 *• ──╾⊱﹝⚜️﹞⊰╼── •*
-> Copyright © 2024 -شـيخ⁵¹⁷.`,subtitle: "Elsony",},header: { hasMediaAttachment: true,...(await prepareWAMessageMedia({ image : { url: Elsony } }, { upload: conn.waUploadToServer }, {quoted: m}))},
+> Copyright © 2024 -ستيڤن.`,subtitle: "Elsony",},header: { hasMediaAttachment: true,...(await prepareWAMessageMedia({ image : { url: Elsony } }, { upload: conn.waUploadToServer }, {quoted: m}))},
                     contextInfo: {
                         mentionedJid: [m.sender],
                         isForwarded: false,
